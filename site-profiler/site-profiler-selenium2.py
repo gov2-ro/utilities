e96e15343c443f23fd72216d2d3be94417dc131c
pax <pax@mioritics.ro>
2023-10-04 13:49:22 +0300

    working

--- site-profiler/site-profiler-selenium2.py
+++ site-profiler/site-profiler-selenium2.py
@@ -1,3 +1,15 @@
+""" 
+add timeout 15s
+ """
+
+# Paths
+domainlist_csv = '../../data/site-profiles/domainlist-institutii-publice.csv'
+domain_column = 'clean'
+results_csv = '../../data/site-profiles/selenium-pings-institutii-publice.csv'
+# Define the number of rows to write to CSV at once
+batch_size = 20
+
+gecko_driver_path = '/usr/local/bin/geckodriver'
 
 import csv
 import time
@@ -15,15 +27,7 @@ import requests
 from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
 
-# Paths
-domainlist_csv = '../../data/site-profiles/domenii-institutii-centrale.csv'
-domain_column = 'clean'
-results_csv = '../../data/site-profiles/selenium-ping.csv'
 
-# Define the number of rows to write to CSV at once
-batch_size = 20
-
-gecko_driver_path = '/usr/local/bin/geckodriver'
 # Function to fetch the webpage and collect data
 def fetch_website_data(driver, domain, row):
     try:
@@ -41,26 +45,53 @@ def fetch_website_data(driver, domain, row):
         final_url = driver.current_url
         response_time_1 = end_time_load - start_time_1
         # response_code = driver.execute_script("return JSON.stringify(window.performance.timing)")
-        response_code = driver.execute_script("return window.performance.timing.responseEnd")
+        # response_code = driver.execute_script("return window.performance.timing.responseEnd")
+        
 
         response_time_load = end_time_load - start_time_1
         page_size = driver.execute_script("return document.body.scrollHeight")
         page_size_words = len(driver.page_source.split())
-        last_updated = time.strftime('%Y-%m-%d %H:%M:%S')
-        fetch_error = None  # No error if page loaded successfully
+        page_title = driver.title
+  
+        # Collect RSS feeds from page source (if any)
+        page_source = driver.page_source
+        rss_feeds = []
+        soup = BeautifulSoup(page_source, 'html.parser')
+        link_tags = soup.find_all('link', type='application/rss+xml')
+        for link_tag in link_tags:
+            rss_feeds.append(link_tag.get('href'))
+
+        # Check for sitemap URL in page source
+        sitemap_url = None
+        try:
+            sitemap_tags = soup.find_all('link', rel='sitemap')
+            if sitemap_tags:
+                sitemap_url = sitemap_tags[0].get('href')
+        except:
+            sitemap_url = None
+
+        # If no sitemap URL found in page source, check {url}/sitemap.xml
+        if not sitemap_url:
+            try:
+                sitemap_test_url = f'http://{domain}/sitemap.xml'
+                sitemap_response = requests.get(sitemap_test_url)
+                if sitemap_response.status_code == 200:
+                    sitemap_url = sitemap_test_url
+            except:
+                sitemap_url = None
 
     except Exception as e:
         # Handle errors
         final_url = None
         response_time_1 = None
-        response_code = None
+        # response_code = None
         response_time_load = None
         page_size = None
         page_size_words = None
-        last_updated = None
-        fetch_error = str(e)
+ 
+        page_title = '-ERR: ' + str(e)
 
-    return [domain, final_url, response_time_1, response_code, response_time_load, page_size, page_size_words, last_updated, fetch_error]
+    return [domain, final_url, response_time_1, response_time_load, page_size, page_size_words, page_title, rss_feeds, sitemap_url]
 
 # Create a Firefox WebDriver with visible browser
 options = Options()
@@ -83,7 +114,7 @@ total_rows = len(rows)
 pbar = tqdm(total=total_rows)
 
 # Write header to results_csv
-header = ["domain", "final_url", "response_time_1", "response_code", "response_time_load", "page_size", "page_size_words", "last_updated", "fetch_error"]
+header = ["domain", "final_url", "response_time_1", "response_time_load", "page_size", "page_size_words",   "page_title", "rss_feeds", "sitemap_url"]
 with open(results_csv, 'w', newline='') as outfile:
     csv_writer = csv.writer(outfile)
     csv_writer.writerow(header)
